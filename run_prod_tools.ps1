dfd70b7e9bec985f644499efcdebb779da63db64
SqrtNegativOne <sqrtnegativ1@gmail.com>
2026-01-27 13:36:47 +0530

    Don't really use that anymore, so.

--- run_prod_tools.ps1
+++ run_prod_tools.ps1
@@ -2,4 +2,3 @@ $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 Set-Location $scriptPath
 
 Start-Process ".\.venv\Scripts\pythonw.exe" ".\src\app_blocker.pyw" -WindowStyle Hidden
-Start-Process ".\.venv\Scripts\pythonw.exe" ".\src\daily_opener.pyw" -WindowStyle Hidden
