d9c6e45fbd3dad53732441e0481655e88960eb15
SqrtNegativOne <sqrtnegativ1@gmail.com>
2026-04-13 07:10:29 +0530

    Update run_prod_tools.ps1

--- run_prod_tools.ps1
+++ run_prod_tools.ps1
@@ -2,3 +2,4 @@ $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 Set-Location $scriptPath
 
 Start-Process ".\.venv\Scripts\pythonw.exe" ".\src\app_blocker.pyw" -WindowStyle Hidden
+Start-Process ".\.venv\Scripts\pythonw.exe" ".\src\taskbar_autohide.pyw" -WindowStyle Hidden
